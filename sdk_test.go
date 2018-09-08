85f20531c1b52efce15b7863fc8589a949dc017f
Marco Vermeulen <vermeulen.mp@gmail.com>
2018-09-08 14:29:37 +0100

    Integrate godog with go test.

--- sdk_test.go
+++ sdk_test.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/DATA-DOG/godog"
+	"github.com/DATA-DOG/godog/colors"
 	"github.com/kami-zh/go-capturer"
-	"log"
+	"os"
 	"strings"
+	"testing"
 )
 
 var actual string
@@ -43,4 +46,25 @@ func FeatureContext(s *godog.Suite) {
 	s.Step(`^the sdkman version is "(.*)"$`, theSdkmanVersionIs)
 	s.Step(`^I enter "(.*)"$`, iEnter)
 	s.Step(`^I see "(.*)"$`, iSee)
+}
+
+// for `go test` integration
+var opt = godog.Options{Output: colors.Colored(os.Stdout)}
+
+func init() {
+	godog.BindFlags("godog.", flag.CommandLine, &opt)
+}
+
+func TestMain(m *testing.M) {
+	flag.Parse()
+	opt.Paths = flag.Args()
+
+	status := godog.RunWithOptions("godogs", func(s *godog.Suite) {
+		FeatureContext(s)
+	}, opt)
+
+	if st := m.Run(); st > status {
+		status = st
+	}
+	os.Exit(status)
 }
